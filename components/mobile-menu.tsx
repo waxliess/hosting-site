3e0915cd486ecaf2c388539e54b620726f9e8931
waxliess <feqangerayli5@gmail.com>
2026-06-27 21:08:50 +0400

    Update mobile-menu.tsx

--- components/mobile-menu.tsx
+++ components/mobile-menu.tsx
@@ -279,7 +279,7 @@ export default function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
                 >
                   <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
                 </svg>
-                <span>+90 888 888 888</span>
+                <span>+994 888 88 88</span>
               </motion.div>
               <motion.div 
                 className="mt-2 flex items-center gap-3 text-white"
@@ -301,7 +301,7 @@ export default function MobileMenu({ isOpen, onClose }: MobileMenuProps) {
                   <rect width="20" height="16" x="2" y="4" rx="2" />
                   <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
                 </svg>
-                <span>contact@erslly.dev</span>
+                <span>contact@feqan.com.tr</span>
               </motion.div>
             </motion.div>
           </motion.div>
